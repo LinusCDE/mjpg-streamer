78b1f170bca53ec66ebec664b06be91fc3260b6f
Linus <wendefelix1998@gmail.com>
2020-02-11 20:38:51 +0100

    static enableUsb() and add copyright notice

--- mjpg-streamer-experimental/plugins/output_http/usb_control.c
+++ mjpg-streamer-experimental/plugins/output_http/usb_control.c
@@ -1,3 +1,25 @@
+/*******************************************************************************
+#                                                                              #
+#      MJPG-streamer allows to stream JPG frames from an input-plugin          #
+#      to several output plugins                                               #
+#                                                                              #
+#      Copyright (C) 2020 Linus K.                                             #
+#                                                                              #
+# This program is free software; you can redistribute it and/or modify         #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation; version 2 of the License.                      #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    #
+#                                                                              #
+*******************************************************************************/
+
 #include "usb_control.h"
 
 #include <stdio.h>
@@ -33,7 +55,7 @@ static unsigned long getMillis() {
     return fullMs;
 }
 
-void enableUsb() {
+static void enableUsb() {
     pthread_mutex_lock(&mutex_global);
     digitalWrite(USB_SWITCH_PIN, 1);
     lastEnabled = getMillis();
